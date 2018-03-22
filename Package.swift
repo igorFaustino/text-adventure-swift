8ae1e0fc83a7a5bec490568c73c256ed30866aac
Igor Neves Faustino <igornfaustino@gmail.com>
2018-03-22 19:22:52 -0300

    fix Package.swift and update README.md

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -8,12 +5,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "text-adventure-swift",
-            dependencies: []),
+       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
     ]
 )
