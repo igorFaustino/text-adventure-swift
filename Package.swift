3703ef0264dc476dd805ac9c65f8104e92594e3c
Igor Neves Faustino <igornfaustino@gmail.com>
2018-03-26 10:54:12 -0300

    add rainbow package

--- Package.swift
+++ Package.swift
@@ -6,5 +6,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
+       .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 3)
     ]
 )
